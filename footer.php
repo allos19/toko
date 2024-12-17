3670920ba04604397109e31a90494b2f2dba4ff1
Alma <allosmamaroh03@gmail.com>
2024-12-17 15:34:37 +0700

    new file:   adminpanel/index.php 	new file:   adminpanel/kategori-detail.php 	new file:   adminpanel/kategori.php 	new file:   adminpanel/login.php 	new file:   adminpanel/logout.php 	new file:   adminpanel/navbar.php 	new file:   adminpanel/produk-detail.php 	new file:   adminpanel/produk.php 	new file:   adminpanel/session.php 	new file:   bootstrap/css/bootstrap.min.css 	new file:   bootstrap/js/bootstrap.bundle.min.js 	new file:   css/style.css 	new file:   fontawesome/css/fontawesome.min.css 	new file:   fontawesome/js/all.min.js 	new file:   footer.php 	new file:   image/10.png 	new file:   image/11.png 	new file:   image/12.png 	new file:   image/13.png 	new file:   image/14.png 	new file:   image/17.png 	new file:   image/19.png 	new file:   image/21.png 	new file:   image/3.png 	new file:   image/4.png 	new file:   image/5.png 	new file:   image/7.png 	new file:   image/7INwQt32qa5723YZZeeB.png 	new file:   image/9.png 	new file:   image/F3cvPUL8ixtEQLbfLlvB.png 	new file:   image/JMAtCgieJ0B512J7xh85.png 	new file:   image/MART7A383fKixnXehnJ3.png 	new file:   image/R4HOpO0cENE2CLXi8w8B.png 	new file:   image/S3ptZrQKLtLBsk1nDEmO.png 	new file:   image/V0nR7ACfpVXzbgqgLO3R.png 	new file:   image/ZZH9LNVhzPudCNXGWCaN.png 	new file:   image/art.png 	new file:   image/babies.png 	new file:   image/fM0aPqeq6h8n8ggr3Qh6.png 	new file:   image/facebook.png 	new file:   image/furniture-decor.png 	new file:   image/google.png 	new file:   image/home-appliance.png 	new file:   image/housewears.png 	new file:   image/mBnhohBY5n5X2pm6dCci.png 	new file:   image/musical-inst.png 	new file:   image/perfumery.png 	new file:   image/shopping.jpg 	new file:   image/sport-leisure.png 	new file:   image/target_corp.jpg 	new file:   image/target_logo.png 	new file:   image/twitter.png 	new file:   index.php 	new file:   keranjang.php 	new file:   koneksi.php 	new file:   navbar.php 	new file:   produk-detail.php 	new file:   produk.php 	new file:   tentang-kami.php

---
+++ footer.php
@@ -0,0 +1,38 @@
+<div class="container-fluid py-5 warna3 text-light">
+    <div class="container">
+        <h5 class="text-center mb-4">Temui Kami</h5>
+        <div class="row justify-content-center">
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="#" class="no-decoration"><i class="fa-brands fa-facebook fs-2"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="#" class="no-decoration"><i class="fa-brands fa-instagram fs-2"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="#" class="no-decoration"><i class="fa-brands fa-twitter fs-2"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="#" class="no-decoration"><i class="fa-brands fa-whatsapp fs-2"></i></a>
+            </div>
+        </div>
+
+        <!-- <h5 class="text-center mt-5 mb-0">Subscribe</h5>
+        <p class="text-center">untuk informasi tentang promo</p>
+
+        <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3 mt-5">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Ketikkan E-mail"
+                aria-label="Recipient's username" aria-describedby="button-addon2">
+                <button class="btn warna1 text-light" type="button"
+                id="button-addon2">Kirim</button>
+            </div>
+        </div> -->
+    </div>
+</div>
+
+<div class="container-fluid py-3 bg-dark text-light">
+    <div class="container d-flex justify-content-between">
+        <label>&copy;2024 Toko Online</label>
+        <label>Created by </label>
+    </div>
+</div>
